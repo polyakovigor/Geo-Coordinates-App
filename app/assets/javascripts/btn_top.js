9cfc328fc856c4b7f062de5a9180c8bba8be5cda
Igor <igor.polyakov@faceit-team.com>
2017-10-24 11:00:22 +0300

    fix errors

--- app/assets/javascripts/btn_top.js
+++ app/assets/javascripts/btn_top.js
@@ -1,9 +1,16 @@
+// When the user scrolls down 300px from the top of the document, show the button
 window.onscroll = function() {scrollFunction()};
+$('#btn_top').onclick = function() {topFunction()};
 
 function scrollFunction() {
-    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    if (document.body.scrollTop > 300 || document.documentElement.scrollTop > 300) {
         document.getElementById("btn_top").style.display = "block";
     } else {
         document.getElementById("btn_top").style.display = "none";
     }
+}
+
+function topFunction() {
+    document.body.scrollTop = 0; // For Chrome, Safari and Opera
+    document.documentElement.scrollTop = 0; // For IE and Firefox
 }
